8295c9bc49d668c10eecc3d2013ac2242842806e
PolyaevMA <polyaev.maks@ya.ru>
2021-12-30 23:36:34 +0300

    Init next js app

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.scss'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  return (Component.getLayout ?? ((page) => page))(<Component {...pageProps} />)
+}
